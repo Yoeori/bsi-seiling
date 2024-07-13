ea9865b99f9cc774b9287edf1bf448694fec1fa3
Yoeri Otten <yoeri@otten.dev>
2024-07-13 21:24:34 +0200

    feat: add ImageHeader to show a nice sailing picture

---
+++ src/components/ImageHeader/ImageHeader.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./ImageHeader.module.css";
+import { ReactNode } from "react";
+
+interface ImageHeaderProps {
+  src: string;
+  alt: string;
+  tiny?: boolean;
+  children?: ReactNode;
+}
+
+export default function ImageHeader({ src, alt, tiny = false, children }: ImageHeaderProps) {
+  return (
+    <div className={styles.container} data-tiny={tiny}>
+      <Image src={src} alt={alt} fill={true} className={styles.image} />
+      {children}
+    </div>
+  );
+}
