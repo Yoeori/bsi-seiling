57006d56a4b4853b5d3d6598b846d214ced6c77a
Yoeri Otten <yoeri@otten.dev>
2024-07-13 23:48:45 +0200

    feat: add simple mdx renderer

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,19 +1,39 @@
+import ImageHeader from "@/components/ImageHeader/ImageHeader";
 import { getPageBySlug } from "@/controllers/page";
+import { Container } from "@mantine/core";
+import { MDXRemote } from "next-mdx-remote/rsc";
 import { notFound } from "next/navigation";
+
+const IMAGES = [
+  {
+    src: "/images/ran-trip.webp",
+    alt: "View from the RAN cabin",
+  },
+  {
+    src: "/images/sailing.jpg",
+    alt: "Schmelnick sailing in the fjord",
+  },
+];
 
 interface PageProps {
   params: {
     slug: string;
   };
-}
+};
 
-
-export default async function Page({ params: { slug }}: PageProps) {
+export default async function Page({ params: { slug } }: PageProps) {
   const page = await getPageBySlug(slug);
 
   if (!page) {
     notFound();
   }
 
-  return <div>{page.content}</div>;
+  const img = IMAGES[1]; // TODO
+
+  return <>
+    <ImageHeader {...img} tiny />
+    <Container>
+      <MDXRemote source={page.content} />
+    </Container>
+  </>
 }
