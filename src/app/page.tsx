6d98cea942cc0a69aed30105e3b4bc178b19f583
Yoeri Otten <yoeri@otten.dev>
2024-07-13 21:25:11 +0200

    feat: initial index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,19 @@
 'use client';
 
-import Image from "next/image";
-import styles from "./page.module.css";
-import { AppShell, Burger } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import ImageHeader from "@/components/ImageHeader/ImageHeader";
+import { Container } from "@mantine/core";
 
 export default function Home() {
-  const [opened, { toggle }] = useDisclosure();
-
-  return (
-    <AppShell
-      header={{ height: 60 }}
-      // navbar={{
-      //   width: 300,
-      //   breakpoint: 'sm',
-      //   collapsed: { mobile: !opened },
-      // }}
-      padding="md"
-    >
-      <AppShell.Header>
-        {/* <Burger
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="sm"
-          size="sm"
-        /> */}
-        <div>Logo</div>
-      </AppShell.Header>
-
-      {/* <AppShell.Navbar p="md">Navbar</AppShell.Navbar> */}
-
-      <AppShell.Main>Main</AppShell.Main>
-    </AppShell>
-  );
+  return <>
+    <ImageHeader src="/images/sailing.jpg" alt="Sailing boat" />
+    <Container>
+      <h1>BSI Seiling</h1>
+      <p>
+        BSI Seiling is a non-profit sports organisation and a part of the BSI sports alliance.
+      </p>
+      <p>
+        We were founded in 1997 and our goal is to promote sailing as a sport and leisure to students and other in the Bergen area, by arranging sailing courses and trips for our members. We currently have two Albin Express boats which are used for daily members sailings. We also have a 31 ft Albin Delta, used for both member sailings and longer trips and weekends. We are based in a quiet marina at Laksevåg, Merkur Boat Club.
+      </p>
+    </Container>
+  </>;
 }
